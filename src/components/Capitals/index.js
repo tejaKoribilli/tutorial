2603bed929c4647c0f2a39b8314e71a28bf01085
thisisteja <tejakoribilli8892@gmail.com>
2023-03-31 03:22:32 +0000

    capitals app react

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,75 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+// Write your code here
+
+class Capitals extends Component {
+  state = {country: 'India'}
+
+  onDropdownChange = event => {
+    const dropDown = event.target.value
+    console.log(dropDown)
+
+    const countryName = countryAndCapitalsList.filter(each =>
+      each.id.includes(dropDown),
+    )
+    this.setState({country: countryName[0].country})
+  }
+
+  render() {
+    const {country} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="main-container">
+          <h1>Countries And Capitals</h1>
+          <div className="label-dropdown">
+            <select
+              name="capitals"
+              className="dropdown"
+              onChange={this.onDropdownChange}
+            >
+              {countryAndCapitalsList.map(each => (
+                <option key={each.id} value={each.id} id={each.id} selected>
+                  {each.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="label-content">is capital of which country?</p>
+          </div>
+          <h1 className="country-name">{country}</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Capitals
