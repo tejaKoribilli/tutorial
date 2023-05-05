d148dd2b3381c0a6c6119ced070f284a7deb9466
thisisteja <tejakoribilli8892@gmail.com>
2023-05-05 05:35:50 +0000

    nxtAuthenticationApp

---
+++ src/components/Cart/index.js
@@ -0,0 +1,26 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="cart-bg-container">
+        <img
+          className="cart-image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,27 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="products-bg-container">
+        <img
+          className="products-image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
